29b69d7606a28f63da2694ef5b6266690ae43787
James Mosier <jamesdmosier@gmail.com>
2014-07-23 20:51:00 -0400

    broken promise..halfway thru 3.6. EditEventController and EventController throw errors. Look at 3.6 using  service around 6:50-ish mark

--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,23 +1,14 @@
 eventsApp.factory('eventData', function ($resource) {
+
+	var resource = $resource('./data/event/:id.json', {id:'@id'});
+
 	return {
 		getEvent: function() {
-			// var deferred = $q.defer();
-			// $http({method: 'GET', url: './data/event/1.json'}).
-			// 	success(function(data, status, headers, config) {
-			// 		//successcb(data);
-			// 		deferred.resolve(data);
-			// }).
-			// 	error(function(data, status, headers, config){
-			// 		//$log.warn(data, status, headers, config);
-			// 		deferred.reject(status);
-			// });
-
-			// return deferred.promise;
-			
-			//replaced code above with $resource
-			return $resource('./data/event/:id.json', {id:'@id'}).get({id:1});
-
-
+			return resource.get({id:1});
+		},
+		save: function(event) {
+			event.id = 999;
+			return resource.save(event);
 		}
 	};
 
